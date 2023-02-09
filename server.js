5c666196b3eac337185fa4c02290075b0767f8e7
Uttanon <uttanon.aug@gmail.com>
2023-02-09 15:18:59 +0700

    connect with mongo db

--- server.js
+++ server.js
@@ -3,11 +3,21 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 const app = express();
 const hospitals = require("./routes/hospitals");
+const connectDB = require("./config/db");
+
+app.use(express.json());
+connectDB();
 
 app.use("/api/v1/hospitals", hospitals);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+
+const server = app.listen(
   PORT,
   console.log("Server running in ", process.env.NODE_ENV, " mode in port", PORT)
 );
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
