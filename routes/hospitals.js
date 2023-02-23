0e428b44233863a80faa686a6c121e2b43005454
Uttanon <uttanon.aug@gmail.com>
2023-02-23 14:57:34 +0700

    add authen

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const {
-  getHospitals,
-  createHospital,
-  getHospital,
-  updateHospital,
-  deleteHospital,
+    getHospitals,
+    createHospital,
+    getHospital,
+    updateHospital,
+    deleteHospital,
 } = require("../controllers/hospital");
+const { protect, authorize } = require("../middleware/auth");
 const router = express.Router();
 const app = express();
 
-router.route("/").get(getHospitals).post(createHospital);
 router
-  .route("/:id")
-  .get(getHospital)
-  .put(updateHospital)
-  .delete(deleteHospital);
+    .route("/")
+    .get(getHospitals)
+    .post(protect, authorize("admin"), createHospital);
+router
+    .route("/:id")
+    .get(getHospital)
+    .put(protect, authorize("admin"), updateHospital)
+    .delete(protect, authorize("admin"), deleteHospital);
 
 module.exports = router;
